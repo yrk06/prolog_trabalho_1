4be99944faa900d3c86fd7d27c06c26ae16d069b
Gabriel Ezequiel <gabriel.ezequiel@pucpr.edu.br>
2021-05-05 19:11:50 -0300

    menu simple add

--- Trabalho1.pl
+++ Trabalho1.pl
@@ -1,3 +1,67 @@
+menu:-
+    write('\e[2J'),
+    nl,
+    write('***************************************'),
+    nl,
+    write('*    DOMÍNIO INSTITUIÇÃO DE ENSINO    *'),
+    nl,
+    write('***************************************'),
+    nl,
+    writeln('Escolha qual a opcao deseja executar: '),
+    nl,
+    writeln('1-Incluir Estudante.'),
+    writeln('2-Incluir Notas do Estudante.'),
+    writeln('3-Localizar Estudante pela Matricula'),
+    writeln('4-Excluir Estudante e suas Notas.'),
+    writeln('5-Relatorio dos Estudantes.'),
+    writeln('6-Salvar Dados em Arquivo.'),
+    writeln('7-Carregar Dados em Arquivo.'),
+    writeln('8-Limpar Banco de Dados.'),
+    writeln('0-Sair.'),
+    nl,
+    writeln('Informe a opcao: '),
+    read(Entrada),
+    nl,
+    Entrada =\= 8,
+    executa( Entrada ),
+    menu.
+
+menu :-
+    nl,
+    write('***************************************'),
+    nl,
+    write('*    DOMÍNIO INSTITUIÇÃO DE ENSINO    *'),
+    nl,
+    write('***************************************'),
+    nl,
+    write('Saindo!'), 
+    nl,
+    abort.
+
+executa( 1 ) :-
+    op1.
+
+executa( 2 ) :- 
+    op2.
+
+executa( 3 ) :-
+    op3.
+
+executa( 4 ) :-
+    op4.
+
+executa( 5 ) :-
+    op5.
+
+executa( 6 ) :-
+    op6.
+
+executa( 7 ) :-
+    op7.
+
+executa( 8 ) :-
+    op8.
+
 
 load :-
     [dadostrabalho1].
