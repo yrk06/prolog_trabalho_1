08bbc211bb3dea878cbacbb5aad481498dfd2da8
Gabriel Ezequiel <gabriel.ezequiel@pucpr.edu.br>
2021-05-06 17:28:12 -0300

    op 3 feita

--- Trabalho1.pl
+++ Trabalho1.pl
@@ -1,5 +1,6 @@
 :- dynamic estudante/4.
 :- dynamic nota/4.
+:- dynamic totalnotas/1.
 
 menu:-
     write('\e[2J'),
@@ -48,7 +49,7 @@ executa( 2 ) :-
     incluir_notas_do_estudante.
 
 executa( 3 ) :-
-    op3.
+    localizar_estudante_pela_matricula.
 
 executa( 4 ) :-
     excluir_estudante_e_suas_notas.
@@ -150,6 +151,76 @@ incluir_notas_do_estudante:-
     nl,
     writeln('Digite algo para voltar:'),
     read(_).
+
+/* op 3 inicio */
+localizar_estudante_pela_matricula:-
+    write('---------------------------------------'),
+    nl,
+    write('Localizar Estudante pela Matricula'),
+    nl,
+    writeln('---------------------------------------'),
+    nl,
+    writeln('Informe a matricula do Estudante: '),
+    read(Matricula),
+    estudante(Matricula,Nome,data(Dia,Mes,Ano),Endereco),
+    writef('matriculo do estudante: %t \n',[Matricula]),
+    writef('Nome do aluno: %t \n',[Nome]),
+    writef('data de nascimento %t / %t / %t \n',[Dia,Mes,Ano]),
+    writef('Endereco do aluno: %t \n',[Endereco]),
+    nl,
+    retractall(quantidade_notas(_)),
+    retractall(total_notas(_)),
+    assert(quantidade_notas(0)),
+    assert(total_notas(0)),
+    suasnotas(Matricula),
+    nl,
+    writeln('Digite algo para voltar:'),
+    read(_).
+
+
+localizar_estudante_pela_matricula:-
+    nl,
+    write('---------------------------------------'),
+    nl,
+    write('Matricula nao existe'),
+    nl,
+    writeln('---------------------------------------'),
+    nl,
+    writeln('Digite algo para voltar:'),
+    read(_).
+
+suasnotas(Matricula):-
+    nota(Matricula,DIS,ANO,NOTA),
+    retract(quantidade_notas(NOTAS_AC)),
+    retract(total_notas(NOTAS_TOTAL)),
+    NEW_NOTAS_AC is NOTAS_AC + 1,
+    NEW_NOTAS_TOTAL is NOTAS_TOTAL + NOTA,
+    assert(quantidade_notas(NEW_NOTAS_AC)),
+    assert(total_notas(NEW_NOTAS_TOTAL)),
+    writef('Disciplina: %t Ano: %t Nota: %t \n',[DIS,ANO,NOTA]),
+    fail.
+
+suasnotas(_):-
+    retract(quantidade_notas(A)),
+    retract(total_notas(B)),
+    div_zero(A,B).
+
+div_zero(0,_):-
+    nl,
+    write('---------------------------------------'),
+    nl,
+    write('Este aluno nao possui Notas'),
+    nl,
+    writeln('---------------------------------------'),
+    nl,
+    writeln('Digite algo para voltar:'),
+    read(_).
+
+div_zero(A,B):-
+    C is B / A,
+    writef('total de notas: %t \n',[A]),
+    writef('Nota media: %t \n',[C]).
+/* op 3 fim */
 
 excluir_estudante_e_suas_notas:-
     write('---------------------------------------'),
